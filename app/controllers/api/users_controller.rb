e7249d6013af34957e07b370e807a1fc31e9890f
Stephen Settle <ssettle3@gmail.com>
2019-10-13 22:56:53 -0400

    Take away create from users controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,5 +1,5 @@
 class Api::UsersController < ApplicationController
-  before_action :authorize_request, except: :create
+  before_action :authorize_request
   before_action :find_user, except: :index
 
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   post '/auth/sign_up', to: 'authentication#sign_up'
 
   namespace :api do
-    resources :users
+    resources :users, only: [:index, :show, :update, :destroy]
     resources :exercises
     resources :workouts
   end
