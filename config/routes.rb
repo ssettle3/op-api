bf48938e77da8e3f03b84fa9018d2c241d840005
Stephen Settle <ssettle3@gmail.com>
2019-10-13 22:52:55 -0400

    Move users controller in the API namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :users
   post '/auth/login', to: 'authentication#login'
   post '/auth/sign_up', to: 'authentication#sign_up'
 
   namespace :api do
+    resources :users
     resources :exercises
     resources :workouts
   end
